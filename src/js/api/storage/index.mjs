77107c16ccb80a59fc0b71d45d82522e7dae0bea
Benjamel <benjamin.meldal@gmail.com>
2023-05-08 21:53:19 +0200

    added localstorage file

--- src/js/api/storage/index.mjs
+++ src/js/api/storage/index.mjs
@@ -2,15 +2,15 @@ export function save(key, value) {
   localStorage.setItem(key, JSON.stringify(value));
 }
 
-export function load(key) {
+export const load = (key) => {
   try {
     const value = localStorage.getItem(key);
     return JSON.parse(value);
   } catch {
     return null;
   }
-}
+};
 
 export function remove(key) {
-  localStorage.remove(key);
+  localStorage.removeItem(key);
 }
